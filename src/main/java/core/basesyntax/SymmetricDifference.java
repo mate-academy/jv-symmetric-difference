8e4eaede22a2775e18553e233feec0182cca0cdc
Тарас <tarashryts@gmail.com>
2019-09-03 19:26:32 +0300

    add hw-102-symmetric

--- src/main/java/core/basesyntax/SymmetricDifference.java
+++ src/main/java/core/basesyntax/SymmetricDifference.java
@@ -1,6 +1,7 @@
 package core.basesyntax;
 
 import java.util.Set;
+import java.util.TreeSet;
 
 /**
  * <p>Реализуйте метод, вычисляющий симметрическую разность двух множеств. Метод должен возвращать
@@ -10,6 +11,17 @@ import java.util.Set;
  */
 public class SymmetricDifference<T> {
     public Set<T> symmetricDifference(Set<? extends T> set1, Set<? extends T> set2) {
-        return null;
+        Set<T> result = new TreeSet<>();
+        for (T temp : set1) {
+            if (!set2.contains(temp)) {
+                result.add(temp);
+            }
+        }
+        for (T temp : set2) {
+            if (!set1.contains(temp)) {
+                result.add(temp);
+            }
+        }
+        return result;
     }
 }
