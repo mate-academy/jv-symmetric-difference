07ad73380232087227746592961548e7595e3054
roman_minevich <283389dv3qsh>
2019-09-04 11:48:24 +0300

    Fixed solution

--- src/main/java/core/basesyntax/SymmetricDifference.java
+++ src/main/java/core/basesyntax/SymmetricDifference.java
@@ -10,9 +10,9 @@ import java.util.Set;
  */
 public class SymmetricDifference<T> {
     public Set<T> symmetricDifference(Set<? extends T> set1, Set<? extends T> set2) {
-        Set<T> difference1 = new HashSet<>((Set<T>) set1);
+        Set<T> difference1 = new HashSet<>(set1);
         difference1.removeAll(set2);
-        Set<T> difference2 = new HashSet<>((Set<T>) set2);
+        Set<T> difference2 = new HashSet<>(set2);
         difference2.removeAll(set1);
         difference1.addAll(difference2);
         return difference1;
