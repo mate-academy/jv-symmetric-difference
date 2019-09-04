2d43f8237a483fcf7e6177f3756f5f9d00d53f42
AdminPC\Admin <admin2k7@gmail.com>
2019-09-04 13:06:10 +0300

    10.2 task added

--- src/main/java/core/basesyntax/SymmetricDifference.java
+++ src/main/java/core/basesyntax/SymmetricDifference.java
@@ -1,5 +1,6 @@
 package core.basesyntax;
 
+import java.util.HashSet;
 import java.util.Set;
 
 /**
@@ -10,6 +11,13 @@ import java.util.Set;
  */
 public class SymmetricDifference<T> {
     public Set<T> symmetricDifference(Set<? extends T> set1, Set<? extends T> set2) {
-        return null;
+        Set<T> numbers = new HashSet<>(set1);
+        numbers.addAll(set2);
+
+        Set<T> tempSet = new HashSet<>(set1);
+        tempSet.retainAll(set2);
+        numbers.removeAll(tempSet);
+
+        return numbers;
     }
 }
