d5ca5003b2ec8f9ce74ad134396a6ff24c8b750f
Serhii Yevtushok <ggazila@gmail.com>
2019-12-19 14:09:13 +0200

    made suggested changes

--- src/main/java/core/basesyntax/SymmetricDifference.java
+++ src/main/java/core/basesyntax/SymmetricDifference.java
@@ -11,8 +11,7 @@ import java.util.Set;
  */
 public class SymmetricDifference<T> {
     public Set<T> symmetricDifference(Set<? extends T> set1, Set<? extends T> set2) {
-        Set<T> result = new HashSet<>();
-        result.addAll(set1);
+        Set<T> result = new HashSet<>(set1);
         result.addAll(set2);
         Set<T> equalsElements = new HashSet<>(set1);
         equalsElements.retainAll(set2);
