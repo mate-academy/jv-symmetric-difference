08fd0236465a924c6a0f150e12378671ff85eab2
Dmitrii <zinchukdimaa@gmail.com>
2019-12-19 18:14:42 +0200

    changed to HashSet

--- src/main/java/core/basesyntax/SymmetricDifference.java
+++ src/main/java/core/basesyntax/SymmetricDifference.java
@@ -1,6 +1,6 @@
 package core.basesyntax;
 
-import java.util.LinkedHashSet;
+import java.util.HashSet;
 import java.util.Set;
 
 /**
@@ -11,8 +11,8 @@ import java.util.Set;
  */
 public class SymmetricDifference<T> {
     public Set<T> symmetricDifference(Set<? extends T> set1, Set<? extends T> set2) {
-        Set<T> listA = new LinkedHashSet<>(set1);
-        Set<T> listB = new LinkedHashSet<>(set2);
+        Set<T> listA = new HashSet<>(set1);
+        Set<T> listB = new HashSet<>(set2);
         listA.removeAll(set2);
         listB.removeAll(set1);
         listA.addAll(listB);
