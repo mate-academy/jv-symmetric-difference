8fdd4308dde42b0d5f9463aaf15c4ebbfef7f5d5
MrNopi <Smart.like1998@gmail.com>
2019-12-19 16:58:37 +0300

    Remade solution with -remove and -addAll

--- src/main/java/core/basesyntax/SymmetricDifference.java
+++ src/main/java/core/basesyntax/SymmetricDifference.java
@@ -10,19 +10,13 @@ import java.util.TreeSet;
  * <p>Пример: симметрическая разность множеств {1, 2, 3} и {0, 1, 2} равна {0, 3}.</p>
  */
 public class SymmetricDifference<T> {
-    private Set<T> set = new TreeSet<>();
 
     public Set<T> symmetricDifference(Set<? extends T> set1, Set<? extends T> set2) {
-        addSymetricDifference(set1, set2);
-        addSymetricDifference(set2, set1);
+        Set<T> set = new TreeSet<>(set1);
+        Set<T> secondSet = new TreeSet<>(set2);
+        secondSet.removeAll(set);
+        set.removeAll(set2);
+        set.addAll(secondSet);
         return set;
-    }
-
-    private void addSymetricDifference(Set<? extends T> set1, Set<? extends T> set2) {
-        for (T element : set1) {
-            if (!set2.contains(element)) {
-                set.add(element);
-            }
-        }
     }
 }
