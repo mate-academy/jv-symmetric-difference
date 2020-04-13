bdb4132e87d0665bc562f1d982f7bdf7ca176bb5
Andrey Tanchak <tanchak12@gmail.com>
2020-04-13 20:29:09 +0300

    added solution 2

--- src/main/java/core/basesyntax/SymmetricDifference.java
+++ src/main/java/core/basesyntax/SymmetricDifference.java
@@ -1,23 +1,16 @@
 package core.basesyntax;
 
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.Set;
 
 public class SymmetricDifference<T> {
 
     public Set<T> symmetricDifference(Set<? extends T> set1, Set<? extends T> set2) {
-        Set<T> a = new HashSet<>(set1);
-        Set<T> b = new HashSet<>(set2);
-        Iterator<T> c = a.iterator();
-        while (c.hasNext()) {
-            T testNext = c.next();
-            if (b.contains(testNext)) {
-                b.remove(testNext);
-            } else {
-                b.add(testNext);
-            }
-        }
-        return b;
+        Set<T> uniqueElements = new HashSet<>();
+        uniqueElements.addAll(set1);
+        uniqueElements.addAll(set2);
+        set2.retainAll(set1);
+        uniqueElements.removeAll(set2);
+        return uniqueElements;
     }
 }
