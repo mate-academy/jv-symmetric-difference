2bba1343d30a5d5473a6c51efa4a2027dafdf683
andriy <maziarandriy@gmail.com>
2020-04-13 12:47:17 +0300

    hw10.2 solution with two variable

--- src/main/java/core/basesyntax/SymmetricDifference.java
+++ src/main/java/core/basesyntax/SymmetricDifference.java
@@ -13,11 +13,9 @@ public class SymmetricDifference<T> {
     public Set<T> symmetricDifference(Set<? extends T> set1, Set<? extends T> set2) {
         Set<T> result1 = new HashSet<>(set1);
         Set<T> result2 = new HashSet<>(set2);
-        Set<T> result = new HashSet<>();
         result1.removeAll(set2);
         result2.removeAll(set1);
-        result.addAll(result1);
-        result.addAll(result2);
-        return result;
+        result1.addAll(result2);
+        return result1;
     }
 }
