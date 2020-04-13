ea239ebfda8a03e5a32b4ba2367c9f1e5e407ebe
Yaroslav-Serhii <u.vusochanskiy@gmail.com>
2020-04-13 14:20:27 +0300

    Renamed variables.

--- src/main/java/core/basesyntax/SymmetricDifference.java
+++ src/main/java/core/basesyntax/SymmetricDifference.java
@@ -12,11 +12,11 @@ import java.util.TreeSet;
  */
 public class SymmetricDifference<T> {
     public Set<T> symmetricDifference(Set<? extends T> set1, Set<? extends T> set2) {
-        Set<T> result1 = new HashSet<>(set1);
-        Set<T> result2 = new HashSet<>(set2);
-        result1.removeAll(set2);
-        result2.removeAll(set1);
-        result1.addAll(result2);
-        return result1;
+        Set<T> internalSet1 = new HashSet<>(set1);
+        Set<T> internalSet2 = new HashSet<>(set2);
+        internalSet1.removeAll(set2);
+        internalSet2.removeAll(set1);
+        internalSet1.addAll(internalSet2);
+        return internalSet1;
     }
 }
