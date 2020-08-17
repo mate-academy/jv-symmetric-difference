5a13bcdec81f0cb4eb58c1d70f2b756dc3fc15b9
margarita-ignatieva <ignatevamargarita950@gmail.com>
2020-08-17 18:47:37 +0300

    constructors added

--- src/main/java/core/basesyntax/SymmetricDifference.java
+++ src/main/java/core/basesyntax/SymmetricDifference.java
@@ -5,11 +5,9 @@ import java.util.Set;
 
 public class SymmetricDifference<T> {
     public Set<T> symmetricDifference(Set<? extends T> set1, Set<? extends T> set2) {
-        Set<T> tempSet = new HashSet<>();
-        Set<T> difference = new HashSet<>();
-        tempSet.addAll(set1);
+        Set<T> tempSet = new HashSet<>(set1);
+        Set<T> difference = new HashSet<>(set2);
         tempSet.removeAll(set2);
-        difference.addAll(set2);
         difference.removeAll(set1);
         difference.addAll(tempSet);
         return difference;
