c99fa3f34ce88c7dbb71994eec0041da8dbd4c69
romanHreits <romagreic@gmail.com>
2020-08-17 13:56:39 +0300

    ready solution hw-16.2

--- src/main/java/core/basesyntax/SymmetricDifference.java
+++ src/main/java/core/basesyntax/SymmetricDifference.java
@@ -1,5 +1,6 @@
 package core.basesyntax;
 
+import java.util.HashSet;
 import java.util.Set;
 
 /**
@@ -10,6 +11,11 @@ import java.util.Set;
  */
 public class SymmetricDifference<T> {
     public Set<T> symmetricDifference(Set<? extends T> set1, Set<? extends T> set2) {
-        return null;
+        Set<T> finalSet = new HashSet<>(set1);
+        finalSet.removeAll(set2);
+        Set<T> temporarySet = new HashSet<>(set2);
+        temporarySet.removeAll(set1);
+        finalSet.addAll(temporarySet);
+        return finalSet;
     }
 }
