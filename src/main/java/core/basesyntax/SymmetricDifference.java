2e328a3dea6f3f344430f28bf0f3f0a0d879b1fe
Dm_Grt <grytsishin@gmail.com>
2020-08-17 17:15:44 +0300

    symmetricDifference implementation added

--- src/main/java/core/basesyntax/SymmetricDifference.java
+++ src/main/java/core/basesyntax/SymmetricDifference.java
@@ -1,15 +1,15 @@
 package core.basesyntax;
 
+import java.util.HashSet;
 import java.util.Set;
 
-/**
- * <p>Реализуйте метод, вычисляющий симметрическую разность двух множеств. Метод должен возвращать
- * результат в виде нового множества.</p>
- *
- * <p>Пример: симметрическая разность множеств {1, 2, 3} и {0, 1, 2} равна {0, 3}.</p>
- */
 public class SymmetricDifference<T> {
     public Set<T> symmetricDifference(Set<? extends T> set1, Set<? extends T> set2) {
-        return null;
+        Set<T> uniqueFromFirst = new HashSet<>(set1);
+        uniqueFromFirst.removeAll(set2);
+        Set<T> uniqFromSecond = new HashSet<>(set2);
+        uniqFromSecond.removeAll(set1);
+        uniqueFromFirst.addAll(uniqFromSecond);
+        return uniqueFromFirst;
     }
 }
