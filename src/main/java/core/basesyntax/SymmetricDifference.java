d806d4dcfcc4c5969a1119966256944199d061b0
Kostian <pyteec.ua@i.ua>
2020-08-17 20:47:01 +0300

    used constructors and renamed my sets

--- src/main/java/core/basesyntax/SymmetricDifference.java
+++ src/main/java/core/basesyntax/SymmetricDifference.java
@@ -11,13 +11,11 @@ import java.util.Set;
  */
 public class SymmetricDifference<T> {
     public Set<T> symmetricDifference(Set<? extends T> set1, Set<? extends T> set2) {
-        Set<T> newSet1 = new HashSet<>();
-        Set<T> newSet2 = new HashSet<>();
-        newSet1.addAll(set1);
-        newSet2.addAll(set2);
-        newSet1.removeAll(set2);
-        newSet2.removeAll(set1);
-        newSet1.addAll(newSet2);
-        return newSet1;
+        Set<T> firstSet = new HashSet<>(set1);
+        Set<T> secondSet = new HashSet<>(set2);
+        firstSet.removeAll(set2);
+        secondSet.removeAll(set1);
+        firstSet.addAll(secondSet);
+        return firstSet;
     }
 }
